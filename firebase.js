258727ab5668da29fc8aa18114af86aa45f2fa0f
vanshikam10 <ervanshikamittal@gmail.com>
2025-05-25 20:45:10 +0530

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 // Firebase config and initialization
 const firebaseConfig = {
-  apiKey: "AIzaSyBnO_wQ6JIbipXPQanTokg0_dsMQWzGfQU",
-  authDomain: "ishqglow.firebaseapp.com",
-  projectId: "ishqglow",
-  storageBucket: "ishqglow.firebasestorage.app",
-  messagingSenderId: "959731860612",
-  appId: "1:959731860612:web:8deabf920e2ae434e3eed1"
+  apiKey: "your api key",
+  authDomain: "your auth domain",
+  projectId: "your project name",
+  storageBucket: "yours storage bucket",
+  messagingSenderId: "your msg sender if",
+  appId: "your app id"
 };
 
 // Initialize Firebase
